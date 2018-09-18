fc8bc464c45c20a1a887f1a10faa40d2450269cf
raouf charef <fa_charef@esi.dz>
2018-09-18 15:47:33 +0200

    Charef : 1ft commit

--- server.js
+++ server.js
@@ -1,81 +1,182 @@
-var express   =   require('express');
-var SIO   =   require('socket.io');
+var express = require('express');
+var SIO = require('socket.io');
 
-var users   =   [];
-var sockets  =  [];
-var conversations = [];
+var users=[];
+var sockets=[];
+var conversation=[];
 
-var app  =  express();
+var app = express();
 
-var server  =  app.listen(3000,function(){
+var server = app.listen(3000,function(){
     console.log("started listening on port 3000");
 });
 
 app.use(express.static("public"));
 
-var io  =  SIO(server);
+var io = SIO(server);
+///***********************************************************************
 
 io.on('connection',function(socket){
     
     //ajouter la socket à la liste des sockets
-    sockets[sockets.length] = socket;
+    sockets[sockets.length]=socket;
+    console.log('a user is connected');
+    
 
-   
+    //ajouter un listner pour les utilisateurs qui ce connect
+    socket.on("newUser",function(data){
+        if(data){
+            users[users.length] = new User(data.name,users.length);
+            socket.broadcast.emit('newUser',{id:socket.id,name : data.name});
+            console.log("new user :"+data.name+", id :"+socket.id);
+            sendListUsers(socket);
+        }
+    });
+
+    //ajouter un listner pour les appels
+    socket.on("call",function(data){
+        //à verifier
+        var i=0;
+        while((i<users.length)&&(data.calleeId!=users[i].id)){i++;}
+        if(i<users.length){
+            socket[i].emit("call",{callerId:socket.id, description:data.description});
+        }else{console.log('l utilisateur n''existe pas')}
+        //....
+    });
+
+    //ajouter un listner pour la reponse à un appel 
+    socket.on('respond',function(data){
+        //à verifier
+        var i=0;
+        while((i<users.length)&&(data.calleeId!=users[i].id)){i++;}
+        if(i<users.length){
+            if(data.answer==true){
+                socket[data.colleeId].emit('respond',colleeId:data.collerId,description:data.description,answer:data.answer);
+            }else{
+                 socket[data.colleeId].emit('respond',colleeId:data.collerId,description:data.description,answer:data.answer);
+            }
+        }
+        //...
+    });
+
+    //ajouter un listner pour le transfert des discription 
+    socket.on('sdp',function (data){
+
+    });
+
+    //ajouter unn  listner pour le transfert des candidat ice
+    socket.on('ice',function(data){
+
+    });
+
+    socket.on('')
 });
 
 
-//creer un nouveau utilisateur de type medecin 
-function newUseer(speciality,firstName, lastName){
-    return {
-        speciality : speciality,
-        firstName : firstName,
-        lastName : lastName
-    };
+//creation d'unn nouveau utilisateur
+function User(name,id){
+    this.name=name;
+    this.id=id;
+}
+
+//chercher une socket par l'id de son propriétaire
+function getSocketByUserID(id){
+    var i=0;
+    while((i<users.length)&&(id!=sockets[i].id)){i++;}
+    if(i<users.length)return sockets[i];
+    return null;
+}
+
+//envoyer la liste des utilisateurs deja connectés au nouveau utilisateur 
+function sendUsersList(socket){
+   users.forEach(user => {
+       socket.emit("newUser",user);
+   };
 }
 
 
-function conversation(initiator){
-    this.initiator = initiator;
-    this.moderator = null;
-    this.participants = [];
+
+
+//creer une nouvel conversassion entre deux ou plus utilisateurs
+function conversation(conversationId,moderatorId){
+    this.conversationId=convId;
+    this.moderatorId=moderatorId;
+    this.participents=[];
+}
+function medecin();
+
+function patient();
+
+function call(callerId,calleeId,description){
+         
+        socket =findSocket(calleeId);
+        socket.emit('call',{callerid:callerid,description:description});
 }
 
-function findSocket(socketId){
-    var i = 0;
-    while((i<socket.length)&&(sockets[i].id != socketId)){i++;}
-    if(i<sockets.length)return socket[i];
-    return false;
-}
+function login(){}
 
-function findConversationById(participentId){
-    var i = 0;
-    while(i<conversations.length){
-        if(inConversation(participentId,conversations[i])){
-            return conversations[i];
+function hangup(collerId){
+
+    var conversattion = findConversationById(callerId);
+    forEach(var participant in conversation.participants){
+       
+       if(participant!=collerId){
+            soket=findSocket(conversation.participants[participant]);
+            socket.emit('hangup',{callerId:callleerId});
         }
     }
-    return false;
-}
-
-function inConversation(participentId,conversation){
-   return (conversation.participants.indexOf(participentId)>0);
-}
-
-function isBusy(UserId){
-    if(findConversationById(UserId)) return true;
-    return false;
-}
-
-function removeUser(userId){
-    users[findUserIndexById(userId)]  =  users[users.length-1];
-    users.pull();
-}
-
-function findUserIndexById(userId){
-    var i  =  0;
-    while ((i<users.length)&&(userId  !=  users[i].id)){i++;}
-    if (i<users.length) return i;
-    return false;
 }
 
 
+function pickup(colleeId,description){
+    if(socket=findSocket(colleeId)){
+          
+          socket.emit('pickup',{description:description,collerId:collerId});
+         
+
+    }
+
+function reject(colleeId)
+
+{
+    if(socket=findSocket(colleeId))
+    {
+       socket.emit('reject',{colleeId:colleeId});
+    }
+
+}
+
+
+function ice(colleeId,){
+    if(socket=findSocket(colleeId)){
+
+         socket.emit('ice',{ice:ice})
+
+    }
+
+}
+
+}
+
+function (colleeId)
+{
+    
+     
+     
+}
+function findSocket();
+function removeUser();
+function userLogout();
+function userLogin();
+function updateList();
+function findUser();
+function dsp(colleeId,description)
+{
+
+
+   socket=findSocket(colleeId);
+   socket.emit()
+
+
+
+}
